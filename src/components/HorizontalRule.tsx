2e4ea98593256da6ce63ce08907fbec6b6d2f73b
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-01-27 01:02:36 +0900

    Feat : add horizontal rule component

---
+++ src/components/HorizontalRule.tsx
@@ -0,0 +1,14 @@
+import {View} from 'react-native';
+import React from 'react';
+
+export const HorizontalRule = () => {
+  return (
+    <View
+      style={{
+        marginTop: 50,
+        borderBottomColor: '#EFEFEF',
+        borderBottomWidth: 1.5,
+      }}
+    />
+  );
+};
