e4cbc8da7c6cfee5f0b791d5a0ac1f6a658aef74
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-01-29 01:15:43 +0900

    Feat : add routing menu labels

--- src/components/RouteLabel.tsx
+++ src/components/RouteLabel.tsx
@@ -1,19 +1,28 @@
 import styled from 'styled-components/native';
+import React from 'react';
 import {Dimensions, Text, TouchableOpacity} from 'react-native';
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 const {width: SCREEN_WIDTH} = Dimensions.get('window');
 const {height: SCREEN_HEIGHT} = Dimensions.get('window');
 
 const Wrapper = styled.View`
-  background-color: #39cb51;
-  flex-direction: column;
+  //background-color: #39cb51;
+  background-color: white;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   margin-right: 8px;
   width: ${SCREEN_WIDTH}px;
   height: 50px;
   border-bottom: white;
-  border-bottom-width: 3px;
+  //border-bottom-width: 3px;
+`;
+
+const LabelText = styled.Text`
+  color: black;
+  font-weight: 500;
+  font-size: 16px;
 `;
 
 interface RouteLabelProps {
@@ -23,10 +32,16 @@ interface RouteLabelProps {
 
 export const RouteLabel = (props: RouteLabelProps) => {
   return (
-    <Wrapper>
-      <TouchableOpacity>
-        <Text>{props.title}</Text>
-      </TouchableOpacity>
-    </Wrapper>
+    <TouchableOpacity>
+      <Wrapper>
+        <LabelText style ={{marginLeft: 28}}>{props.title}</LabelText>
+        <MaterialIcons
+          name={'arrow-forward-ios'}
+          style ={{marginRight: 27}}
+          size={25}
+          color={'#C1CCDA'}
+        />
+      </Wrapper>
+    </TouchableOpacity>
   );
 };
