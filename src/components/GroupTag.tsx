8ac9e8381efab0983caa9b82b7e5238559cd0a69
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-02-08 10:48:57 +0900

    Feat : add group tag component

---
+++ src/components/GroupTag.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+import React, {useState} from 'react';
+import {Text} from 'react-native';
+
+const Wrapper = styled.View`
+  background-color: white;
+  //background-color: #A0DB90;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-right: 8px;
+  border-radius: 10px;
+  border: #e6e6e6;
+`;
+
+const FilterText = styled.Text`
+  color: #808080;
+  font-size: 8px;
+  margin: 2px 7px;
+`;
+
+interface FilterProps {
+  name: string;
+}
+
+const GroupTag = (props: FilterProps) => {
+  return (
+    <Wrapper>
+      <FilterText>{props.name}</FilterText>
+    </Wrapper>
+  );
+};
+
+export default GroupTag;
